f971b3cfac6557ba6a30c6a228eafb012a931761
isx47320900 <isx47320900@i03.informatica.escoladeltreball.org>
2022-03-02 08:52:50 +0100

    ya va serv y clie

---
+++ kclient22/startup.sh
@@ -0,0 +1,2 @@
+#! /bin/bash
+mv krb5.conf /etc/krb5.conf
---
+++ kserver22/startup.sh
@@ -0,0 +1,15 @@
+#! /bin/bash
+mv kadm5.acl /etc/krb5kdc/kadm5.acl
+mv krb5.conf /etc/krb5.conf
+mv ldap.conf /etc/ldap.conf
+mv kdc.conf /etc/krb5kdc/kdc.conf
+
+kdb5_util create -s -P masterkey
+
+for user in anna pere marta jordi pau user{01..10} 
+do
+  kadmin.local -q "addprinc -pw k$user $user"
+done 
+
+/usr/sbin/krb5kdc
+/usr/sbin/kadmind -nofork
